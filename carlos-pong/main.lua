2d568663807304cab6ece367218af3cc7e16c641
Carlos Ruiz <carlosbustillo99@gmail.com>
2022-12-27 13:59:43 -0600

    [ADD] pong 7

--- carlos-pong/main.lua
+++ carlos-pong/main.lua
@@ -51,6 +51,44 @@ function love.load()
 end
 
 function love.update(dt)
+
+  if ball:collides(player1) then
+    ball.dx = -ball.dx * 1.03
+    ball.x = player1.x + 5
+
+    if ball.dy < 0 then
+      ball.dy = -math.random(10, 150)
+    else
+      ball.dy = math.random(10, 150)
+    end
+
+  end
+
+  if ball:collides(player2) then
+    ball.dx = -ball.dx * 1.03
+    ball.x = player2.x - 4
+
+    if ball.dy < 0 then
+      ball.dy = -math.random(10, 150)
+    else
+      ball.dy = math.random(10, 150)
+
+    end
+
+  end
+
+  if ball.y <= 0 then
+    ball.y = 0
+    ball.dy = -ball.dy
+  end
+
+  if ball.y >= VIRTUAL_HEIGHT - 4 then
+    ball.y = VIRTUAL_HEIGHT - 4
+    ball.dy = -ball.dy
+  end
+end
+
+
     -- player 1 movement
     if love.keyboard.isDown('w') then
         player1.dy = -PADDLE_SPEED
@@ -78,6 +116,7 @@ function love.update(dt)
     player1:update(dt)
     player2:update(dt)
 end
+
 
 --[[
     Keyboard handling, called by LÖVE2D each frame; 
