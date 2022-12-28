ca2e189139265510e772cc084b85859efda19ede
Carlos Ruiz <carlosbustillo99@gmail.com>
2022-12-28 09:45:25 -0600

    [fix] love 11

--- carlos-pong/main.lua
+++ carlos-pong/main.lua
@@ -28,6 +28,12 @@ function love.load()
 
   -- set LÖVE2D's active font to the smallFont object
   love.graphics.setFont(smallFont)
+  
+  sounds = {
+    ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+    ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+    ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
 
   -- initialize window with virtual resolution
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -71,6 +77,7 @@ function love.update(dt)
       ball.dy = math.random(10, 150)
     end
 
+    sounds['paddle_hit']:play()
   end
 
   if ball:collides(player2) then
@@ -82,23 +89,27 @@ function love.update(dt)
     else
       ball.dy = math.random(10, 150)
 
-    end
+  end
+      sounds['paddle_hit']:play()
 
   end
 
   if ball.y <= 0 then
     ball.y = 0
     ball.dy = -ball.dy
+    sounds['wall_hit']:play()
   end
 
   if ball.y >= VIRTUAL_HEIGHT - 4 then
     ball.y = VIRTUAL_HEIGHT - 4
     ball.dy = -ball.dy
+    sounds['wall_hit']:play()
   end
   
   if ball.x < 0 then
     servingPlayer = 1
     player2Score = player2Score + 1
+    sounds['score']:play()
     
     if player2Score == 10 then
       winningPlayer = 2
@@ -112,6 +123,7 @@ function love.update(dt)
   if ball.x > VIRTUAL_WIDTH then
     servingPlayer = 2
     player1Score = player1Score + 1
+    sounds['score']:play()
     
     if player1Score == 10 then
       winningPlayer = 1
@@ -122,22 +134,33 @@ function love.update(dt)
     end
   end
 end
-  
 
-
-if ball.x < 0 then
-  servingPlayer = 1
-  player2Score = player2Score + 1
-  ball:reset()
-  gameState = 'start'
-end
-
-if ball.x > VIRTUAL_WIDTH then
-  servingPlayer = 2
-  player1Score = player1Score + 1
-  ball:reset()
-  gameState = 'start'
-end
+    if ball.x < 0 then
+      servingPlayer = 1
+      player2Score = player2Score + 1
+      sounds['score']:play()
+      
+      if player2Score == 10 then
+        winningPlayer = 2
+        gameState = 'done'
+      else
+        gameState = 'serve'
+        ball:reset()
+      end
+    end
+    
+    if ball.x > VIRTUAL_WIDTH then
+      servingPlayer = 2
+      player1Score = player1Score + 1
+      sounds['score']:play()
+      
+      if player1Score == 10 then winningPlayer = 1
+        gameState = 'done'
+      else
+        gameState = 'serve'
+        ball:reset()
+      end
+    end
 
     -- player 1 movement
     if love.keyboard.isDown('w') then
